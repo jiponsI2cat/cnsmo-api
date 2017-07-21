d21fee85b6cd92c484a9ede5b5ba30694d1efd13
wfriscina <william.friscina@i2cat.net>
2017-07-21 12:58:07 +0200

    test

--- controllers/services.js
+++ controllers/services.js
@@ -10,31 +10,20 @@ var cnsmoClient = core.helpers.cnsmoClient;
  * (TODO: eventually it checks if exists client machine)
  */
 function configureFirewall(req, res) {
-  /*   const data = req.body;*/
-
-
-  // send(res, code, message) :::::::::::>>>>>>>>>>> res.body(message)
-  const testData = {
-    direction: 'out',
-    protocol: 'tcp',
-    dst_port: '80',
-    dst_src: 'dst',
-    ip_range: '10.217.123.7/20',
-    action: 'acpt'
-  };
-  cnsmoClient.post('http://127.0.0.1:20095/fw/', testData).then(function(result) {
-    console.log(result);
-    let respns = result.response;
-    respns = 'msgRspsn';
-    return send(res, result.response.statusCode, respns);
-  }).catch(function(err) {
-    console.log(err);
-    const error = {
-      code: 500,
-      message: 'Error!'
-    };
-    return send(res, error.code, error.message);
-  });
+  const bodyReq = req.body;
+  cnsmoClient.post('http://127.0.0.1:20095/fw/', bodyReq)
+    .then(function(result) {
+      console.log(result);
+      const response = result.response;
+      return send(res, response.statusCode, '');
+    }).catch(function(err) {
+      console.log(err);
+      const error = {
+        code: 500,
+        message: 'Error!'
+      };
+      return send(res, error.code, error.message);
+    });
 }
 
 module.exports = {
