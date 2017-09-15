9d06fdc9c4fa4e776fc3a9c054b395819c5fc112
wfriscina <william.friscina@i2cat.net>
2017-09-15 12:40:00 +0200

    Refactor code of getBlockedPorts

--- core/helpers/cnsmo_client.js
+++ core/helpers/cnsmo_client.js
@@ -1,102 +1,30 @@
 'use strict';
-/*
+
 var log4js = require('log4js');
 var Q = require('q');
 var logger = log4js.getLogger('CNSMO CLIENT');
 var Client = require('node-rest-client').Client;
+var Request = require('request');
 
-function getVSD(header, arg, auth, url) {
+const header = {};
+header['Accept'] = 'application/json';
+header['Content-Type'] = 'application/json';
+
+/**
+ * Client GET method
+ * @param {string} url url of server API
+ */
+function get(url, data) {
   var client = new Client();
   var deferred = Q.defer();
   logger.debug('url:' + url);
-
-  header['Authorization'] = 'XREST ' + auth;
+  console.log(data);
   var args = {
-    arg: args,
-    headers: header
+    headers: header,
+    data: data
   };
-
-  logger.debug('args:' + JSON.stringify(args));
-  logger.debug('options:' + JSON.stringify(auth));
 
   client.get(url, args, (data, response) => {
-    onData(data, response, deferred);
-  }).on('error', function(err) {
-    onError(deferred, err);
-  });
-
-  return deferred.promise;
-}
-
-function deleteVSD(header, arg, auth, url, data) {
-  var client = new Client();
-  var deferred = Q.defer();
-  logger.debug('url:' + url);
-
-  header['Authorization'] = 'XREST ' + auth;
-  var args = {
-    arg: args,
-    headers: header
-  };
-
-  logger.debug('args:' + JSON.stringify(args));
-  logger.debug('options:' + JSON.stringify(auth));
-
-  client.delete(url, args, (data, response) => {
-    onData(data, response, deferred);
-  }).on('error', function(err) {
-    onError(deferred, err);
-  });
-
-  return deferred.promise;
-}
-
-function postVSD(header, arg, auth, url, data) {
- var client = new Client();
-  var deferred = Q.defer();
-  logger.debug('url:' + url);
-
-  header['Authorization'] = 'XREST ' + auth;
-  header['X-Requested-With'] = 'XMLHttpRequest';
-  header['Content-Type'] = 'application/json';
-
-  var args = {
-    arg: args,
-    headers: header,
-    data: data
-  };
-
-  logger.debug('args:' + JSON.stringify(args));
-  logger.debug('options:' + JSON.stringify(auth));
-
-  client.post(url, args, (data, response) => {
-    onData(data, response, deferred);
-  }).on('error', function(err) {
-    onError(deferred, err);
-  });
-
-  return deferred.promise;
-}
-
-function putVSD(header, arg, auth, url, data) {
-  var client = new Client();
-  var deferred = Q.defer();
-  logger.debug('put url:' + url);
-
-  header['Authorization'] = 'XREST ' + auth;
-  header['X-Requested-With'] = 'XMLHttpRequest';
-  header['Content-Type'] = 'application/json';
-
-  var args = {
-    arg: args,
-    headers: header,
-    data: data
-  };
-
-  logger.debug('update args:' + JSON.stringify(args));
-  logger.debug('update options:' + JSON.stringify(auth));
-
-  client.put(url, args, (data, response) => {
     onData(data, response, deferred);
   }).on('error', (err) => {
     onError(deferred, err);
@@ -105,10 +33,62 @@ function putVSD(header, arg, auth, url, data) {
   return deferred.promise;
 }
 
+/**
+ * Client POST method
+ * @param {string} url url of server API
+ * @param {object} data data that will be passed in body
+ * request
+ */
+function post(url, data) {
+  var deferred = Q.defer();
+  logger.debug('url:' + url);
+
+  var options = {
+    url: url,
+    headers: header,
+    body: JSON.stringify(data)
+  };
+
+  Request.post(options,
+    (error, response, body) => {
+      console.log(error);
+      if (error) {
+        onError(deferred, error);
+      }
+      onData(body, response, deferred);
+
+    });
+
+  return deferred.promise;
+}
+
+function put(url, data) {
+  var deferred = Q.defer();
+  logger.debug('url:' + url);
+
+  var options = {
+    url: url,
+    headers: header,
+    body: JSON.stringify(data)
+  };
+
+  Request.put(options,
+    (error, response, body) => {
+      console.log(error);
+      if (error) {
+        onError(deferred, error);
+      }
+      onData(body, response, deferred);
+
+    });
+
+  return deferred.promise;
+}
+
 function onData(data, response, deferred) {
   data = (data instanceof Buffer) ? null : data;
-  logger.debug('response from vsd client' + JSON.stringify(data));
-  deferred.resolve({data: data, response: response});
+  logger.debug('response from cnsmo client ' + JSON.stringify(data));
+  deferred.resolve({ data: data, response: response });
 }
 
 function onError(deferred, err) {
@@ -118,7 +98,10 @@ function onError(deferred, err) {
 }
 
 module.exports = {
-
+  get: get,
+  post: post,
+  put: put/*,
+  remove: remove */
 };
 
-*/
+
